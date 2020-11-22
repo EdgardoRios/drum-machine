d8effb7b9611ea739b5ccf4d8525d6106bbce2d9
Edgardo Rios <edgardo.rios@protonmail.com>
2020-11-22 17:38:40 -0600

    Updates

---
+++ precache-manifest.d368cb6073f94d9f174499b8fa8856e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bab1348b54d4b619d1254fe0cde4871",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "4d77d3d950c76e4ecdeb",
+    "url": "/drum-machine/static/css/main.95826d6b.chunk.css"
+  },
+  {
+    "revision": "f086ce68f4e50a6552b6",
+    "url": "/drum-machine/static/js/2.64ece368.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.64ece368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d77d3d950c76e4ecdeb",
+    "url": "/drum-machine/static/js/main.586b8997.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
